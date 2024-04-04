1c8d9b5f99e32aeff9023f98d8fda75ef9b6ea75
Tony Fettes <feihaoxiang2014@gmail.com>
2024-04-05 03:25:48 +0800

    Add checksums and resource blocks

--- Formula/moonbit.rb
+++ Formula/moonbit.rb
@@ -9,32 +9,51 @@ class Moonbit < Formula
     on_arm do
       resource "moon" do
         url "https://cli.moonbitlang.com/macos_m1/moon"
+        sha256 "1c306c51750b8fa988898259ef7d5c94f2cf2e50c93fcfb0422b6a80f1b91772"
       end
       resource "moonc" do
         url "https://cli.moonbitlang.com/macos_m1/moonc"
+        sha256 "26db339647e5ac6df99afcafc57fe5c33bd2cb275092a3b6cab0035c227ccb6d"
       end
       resource "moondoc" do
         url "https://cli.moonbitlang.com/macos_m1/moondoc"
+        sha256 "37c1a0cdf3f4b98872834925fdc44952aee53fe7b8e80e60efbe22dc95b7d24b"
       end
       resource "moonfmt" do
         url "https://cli.moonbitlang.com/macos_m1/moonfmt"
+        sha256 "c56d4a9b11d007f5b364452b8d2ffa772cc7cce126073080b9e555cab72ec026"
       end
       resource "moonrun" do
         url "https://cli.moonbitlang.com/macos_m1/moonrun"
+        sha256 "d66b306077164c5c73e889dbf40e6c57c47a803e8d03f2d5c6cdc27d7cf910a9"
       end
       resource "mooninfo" do
         url "https://cli.moonbitlang.com/macos_m1/mooninfo"
+        sha256 "512ea81bfdc49c4c67c329da1bfe7f2d4bba65e107b5b37893907badf753a29e"
       end
     end
   end
 
   def install
-    libexec.install "moon"
+    resource("moon").stage do
+      libexec.install "moon"
+    end
     bin.env_script_all_files(libexec, MOON_HOME: pkgshare)
-    bin.install "moondoc"
-    bin.install "moonfmt"
-    bin.install "moonrun"
-    bin.install "mooninfo"
+    resource("moonc").stage do
+      bin.install "moonc"
+    end
+    resource("moondoc").stage do
+      bin.install "moondoc"
+    end
+    resource("moonfmt").stage do
+      bin.install "moonfmt"
+    end
+    resource("moonrun").stage do
+      bin.install "moonrun"
+    end
+    resource("mooninfo").stage do
+      bin.install "mooninfo"
+    end
     (pkgshare/"lib").install "core.zip"
   end
 
