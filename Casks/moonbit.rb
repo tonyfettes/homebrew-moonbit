589bca37cc46f3cd492c6c91e491bd02ebe98aec
Tony Fettes <feihaoxiang2014@gmail.com>
2024-05-05 20:24:12 +0800

    feat: cask

---
+++ Casks/moonbit.rb
@@ -0,0 +1,13 @@
+cask "moonbit" do
+  arch arm:   "macos_m1",
+       intel: "macos_intel"
+  version "20240422"
+  url "https://cli.moonbitlang.com/#{arch}/moon"
+  sha256 arm:   "5dd728c35a1d6222374324e066cdb444b42e649924b68bae3f4106e33e065efc",
+         intel: "98cdeb15409ab70e667ecade8e1cc966e5250074d71079de85c69534e5b3a081"
+  name "MoonBit"
+  desc "Intelligent developer platform for Cloud and Edge using WASM"
+  homepage "https://www.moonbitlang.com/"
+
+  binary "moon"
+end
